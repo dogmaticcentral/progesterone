94a1296fb2a1ade82efefc164d9ccdef5847bd0e
Ivana Mihalek <ivana@bigmac.local>
2015-11-28 22:29:48 +0100

    mapped PR binding sites

--- PR_binding_sites/scripts/01_get_gene_regions_from_UCSC.py
+++ PR_binding_sites/scripts/01_get_gene_regions_from_UCSC.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import MySQLdb
 
@@ -55,16 +55,22 @@ def main():
     cursor = db.cursor()
     
     switch_to_db(cursor, "mm9") # mouse build name
-    
-    for chrom in range(1,20):
-        qry = "select refGene.name, refGene.chrom, refGene.name2  from refGene, gbCdnaInfo "
-        qry += "where chrom='chr%s' " % chrom
-        qry += "and gbCdnaInfo.acc=refGene.name  and gbCdnaInfo.mol='mRNA' "
-        qry += "limit 10"
+    # no Y chromosome, we are looking at uterus tissue
+    chromosomes = ["chr"+str(x) for x in range(1,20)] + ["chrX"]
+    for chrom in chromosomes:
+        print "downloading data for", chrom
+        outf = open("gene_ranges."+chrom+".csv", "w")
+        print  >>outf,  "\t".join( ["name", "name2", "txStart", "txEnd"] )
+        qry  = "select g.name,  g.name2, g.txStart, g.txEnd "
+        qry += "from refGene as g, gbCdnaInfo as i, refSeqStatus as s "
+        qry += "where chrom='%s' " % chrom
+        qry += "and i.acc=g.name  and i.type='mRNA' and i.mol='mRNA' "
+        qry += "and s.mrnaAcc=g.name and s.status = 'Validated' "
         rows = search_db(cursor,qry)
-        for row in rows[:10]:
-            print row
- 
+        
+        for row in rows:
+            print  >>outf,  "\t".join( [ str(r) for r in row] )
+        outf.close()
     cursor.close()
     db.close()
 
---
+++ PR_binding_sites/scripts/03_compare_tables.rb
@@ -0,0 +1,98 @@
+#!/usr/bin/env ruby
+
+require_relative 'ruby_modules/region'
+require_relative 'ruby_modules/parsers'
+include Parsers
+
+$verbose = false
+$scratch_space = "/tmp"
+
+##################################
+# & is not applicable to this case, bcs two regions are equivalent if they
+# overlap, not if they have the same from and to (so which one of them should be
+# used as the representative in the intersection?)
+def find_overlapping_regions regions_list_1, regions_list_2
+     overlaps = []
+     uniq_for_list_1 = []
+     regions_list_1.each do |region_1|
+          overlap = regions_list_2.select { |region_2|  region_2  == region_1 }
+          overlap.empty? ? uniq_for_list_1.push(region_1) :  overlaps.push( [region_1, overlap])
+     end
+     return overlaps, uniq_for_list_1
+end
+
+##################################
+# read in the regions detected in the presence of P4 and oil only
+$oil_chrom = parse_chipseq_table "data/GSM857545_1_PR_oil_s_4_aligned.csv"
+$p4_chrom  = parse_chipseq_table "data/GSM857546_2_PR_P4_s_1_aligned.csv"
+
+different_keys =  $p4_chrom.keys - $oil_chrom.keys
+if not different_keys.empty? then abort "oil and P4 do not seem to have the same chromosomes" end
+
+# find ovelaps per chromosome
+uniq_for_p4 = {}
+$p4_chrom.each do |chrom,regions|
+     overlaps, uniq_for_p4[chrom] = find_overlapping_regions regions, $oil_chrom[chrom]
+end
+
+# find overlaps between regions that pop up only in the presence of P4
+chromosomes = (1..19).map { |i| i.to_s} + ['X']
+total_genes_hit = 0
+chromosomes.each do |chrom|
+     
+     gene_data, gene_ranges  = parse_gene_table "data/gene_ranges.chr"+chrom+ ".csv"
+     overlaps, genes_not_hit = find_overlapping_regions gene_ranges, uniq_for_p4[chrom]
+
+     genes_hit = {}
+     
+     overlaps.each do |overlap|
+          gene_region, pr_ranges = overlap
+          name, name2, txStart, txEnd, exonStarts, exonEnds  = gene_data[gene_region].split "\t"
+          if not genes_hit.key? name2
+               genes_hit[name2] = { splices: [[name, txStart.to_i, txEnd.to_i]],
+                    pr_binding_sites:  pr_ranges}
+          else
+               genes_hit[name2][:splices].push [name, txStart.to_i, txEnd.to_i]
+               pr_ranges.each do |this_region|
+                    ranges_seen = genes_hit[name2][:pr_binding_sites].select { |prb| prb === this_region }
+                    if ranges_seen.empty? then genes_hit[name2][:pr_binding_sites].push(this_region) end
+               end
+          end
+     end
+
+     if $verbose
+          puts "chrom #{chrom}  PR binding regions uniq to P4 #{uniq_for_p4[chrom].length}  "
+          puts "         genes:  #{gene_data.length}  "
+          puts "         genes hit:  #{genes_hit.keys.length}  "
+     else
+          outstr = ""
+     end
+     total_genes_hit += genes_hit.length
+
+     genes_hit.each do |name, hash|
+          # the first element of each splice_data array is txStart
+          origin = hash[:splices].map { |splice_data| splice_data[1] }.min
+          rev_origin = hash[:splices].map { |splice_data| splice_data[2] }.max
+          if $verbose
+               puts "="*20
+               puts " #{name}    starts at: #{origin} "
+               puts " splices: "
+               hash[:splices].each do |splice_data|
+                    splice_name, txStart, txEnd = splice_data
+                    puts " \t #{splice_name}    #{txStart-origin}   #{txEnd - origin}  "
+               end
+               puts " PR binding sites: "
+               hash[:pr_binding_sites].each do |pr_region|
+                    puts " \t #{pr_region.from-origin}    #{pr_region.to-origin}  "
+               end
+          else
+               outstr +=   "#{name} #{origin} #{rev_origin} "
+               outstr +=   hash[:splices].map { |splice_data| splice_data[0] }.join","
+               outstr +=  "  "
+               outstr +=   hash[:pr_binding_sites].map {|bs|  "#{bs.from-origin}..#{bs.to-origin}" }.join","
+               outstr +=  "\n"
+          end
+     end
+     File.write($scratch_space+'/p4_chr'+chrom+'.txt', outstr)
+end     
+puts total_genes_hit
